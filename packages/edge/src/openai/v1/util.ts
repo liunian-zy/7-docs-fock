2d95e3888746011d8f67ac748c300bb367a9aa33
liunian-zy <a@liunian.io>
2023-10-31 00:06:59 +0800

    update

--- packages/edge/src/openai/v1/util.ts
+++ packages/edge/src/openai/v1/util.ts
@@ -4,7 +4,10 @@ const chatCompletionModels = [
   'gpt-4-32k',
   'gpt-4-32k-0314',
   'gpt-3.5-turbo',
-  'gpt-3.5-turbo-0301'
+  'gpt-3.5-turbo-0301',
+  'gpt-3.5-turbo-16k-0613',
+  'gpt-3.5-turbo-16k',
+  'gpt-3.5-turbo-0613'
 ];
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
